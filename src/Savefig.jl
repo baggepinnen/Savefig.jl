5efa5d49927dd276af44b698409feac581dc0b04
Fredrik Bagge Carlson <cont-frb@ulund.org>
2018-08-26 17:57:26 +0200

    initial commit

--- src/Savefig.jl
+++ src/Savefig.jl
@@ -1,5 +1,25 @@
 module Savefig
+using Plots
+pgfplots()
 
-greet() = print("Hello World!")
+function savefig_pgfplots(filename)
+    savefig(filename)
+    temppath, temp = mktemp()
+    pattern = r",\s*axis background/.style={fill={rgb,1:red,\d.\d+;green,\d.\d+;blue,\d.\d+}}"
+
+    text = read(filename,String)
+    text = replace(text, pattern => "")
+    text = replace(text, r"width = {[\d.]+mm}" => "")
+    text = replace(text, r"height = {[\d.]+mm}" => "height = \\figureheight, width = \\figurewidth")
+    text = replace(text, r"width = {[\d.]+mm}" => "")
+    println(temp, text)
+
+    close(temp)
+    mv(temppath, filename, remove_destination=true)
+end
 
 end # module
+
+# cd(@__DIR__)
+# plot(randn(100), label="label", title="title", xlabel="xlabel", ylabel="ylabel")
+# savefig_pgfplots("test.tex")
